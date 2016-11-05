5712d67abce960771a751cecf5026b61540b5f43
TillKoeln <TillKoeln>
2016-11-05 03:06:22 +0100

    fix

--- src/version.h
+++ src/version.h
@@ -34,11 +34,11 @@ static const int PROTOCOL_VERSION = 60014;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
-static const int MIN_PROTO_VERSION_FORK = 60014;  
+ 
 
 // disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = 209;
-
+static const int MIN_PROTO_VERSION_FORK = 60014; 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
 static const int CADDR_TIME_VERSION = 31402;
